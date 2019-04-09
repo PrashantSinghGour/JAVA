9eca0ec52a1f693b736251608b225ece0e4f85d4
Prashant Singh Gour <47054841+PrashantSinghGour@users.noreply.github.com>
2019-04-10 00:54:39 +0530

    New Folder Added for Practice Codes
    Here i will store all the java practice codes.

---
+++ Practice Codes/ThreeMultiple.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class ThreeMultiple
+{
+    public static void main(String arg[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Number Whoes 3 Multiple You want to find: ");
+        int Number=sc.nextInt();
+        int i=1,j=1,k=1;
+        while(i<=Number)
+        {
+            while(j<=Number)
+            {
+                while(k<=Number)
+                {
+                    if((i*j*k)==Number)
+                    {
+                        System.out.println(i+" x "+j+" x "+k+" = "+Number);
+                    }
+                    k++;
+                }
+                k=j+1;
+                j++;
+            }
+            j=i;
+            i++;
+           
+        }
+      sc.close();
+    }
+}
