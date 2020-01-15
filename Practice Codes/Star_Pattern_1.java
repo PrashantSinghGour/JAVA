1a986a9b1936761bc64c2faccde23a3186ba2cf8
Prashant Singh Gour <47054841+PrashantSinghGour@users.noreply.github.com>
2020-01-15 19:18:07 +0530

    update

--- Practice Codes/Star_Pattern_1.java
+++ Practice Codes/Star_Pattern_1.java
@@ -34,3 +34,33 @@ class Solution{
        
     }
 }
+
+//pattern2
+
+1	
+2	6	
+3	7	10	
+4	8	11	13	
+5	9	12	14	15	
+
+//logic
+public static void main(String arg[])
+	{
+		int diff;
+		int n;
+		for (int i=1;i<=5;i++)
+		{
+			diff=5;
+			n=i;
+			for(int j=1;j<=i;j++)
+			{
+				diff--;
+				System.out.print(n);
+				System.out.print("\t");
+				n+=diff;	
+			}
+			System.out.println("");
+		}
+	}
+
+
